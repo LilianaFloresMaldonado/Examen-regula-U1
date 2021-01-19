b5d9394558d88a266323f04a335a743b905b903f
LilianaFloresMaldonado <71808888+LilianaFloresMaldonado@users.noreply.github.com>
2021-01-18 18:07:59 -0600

    Update Examenunisdad1.java

--- examenunisdad1/Examenunisdad1.java
+++ examenunisdad1/Examenunisdad1.java
@@ -8,28 +8,39 @@ public class Examenunisdad1 {
 
     
     public static void main(String[] args) {
-          ArrayList<String> lista = new ArrayList<>();
+     ArrayList<String> lista = new ArrayList<>();
         Scanner leer = new Scanner(System.in);
         String respuesta;
-        int tam;
-        boolean agregar = false;
-        agregar = false;
-            System.out.println("Longitud del arreglo:");
-            tam = leer.nextInt();
-            String arreglo[] = new String[tam];
-        do {
- 
-            for (int i = 0; i < arreglo.length; i++) {
-                System.out.println("" + i + "Ingresa el dato: ");
-                arreglo[i] = leer.next();
-                lista.add(arreglo[i]);
-            }
-            System.out.println("¿desea agregar mas elementos? (SI/NO)");
-            respuesta = leer.next();
-            if (respuesta.equalsIgnoreCase("SI")) {
-                agregar = true;
-            }
-        } while (agregar == true);
+        int tamano;
+        boolean parar = false;
+        System.out.println("Longitud del arreglo");
+        tamano= leer.nextInt();
+        String arreglo[] = new String[tamano];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingresa el dato: ");
+            arreglo[i] = leer.next();
+            lista.add(arreglo[i]);
+        }
+        System.out.println("¿desea agregar mas datos? (SI/NO)");
+        respuesta = leer.next();
+        if (respuesta.equalsIgnoreCase("SI")) {
+            do {
+                parar = false;
+                for (int i = 0; i < arreglo.length; i++) {
+                    System.out.println("Dame el dato: ");
+                    arreglo[i] = leer.next();
+                    lista.add(arreglo[i]);
+                    System.out.println("¿desea agregar mas datos? (SI/NO)");
+                    respuesta = leer.next();
+                    if (respuesta.equalsIgnoreCase("SI")) {
+                        parar = true;
+                    } else {
+                        parar = false;
+                        break;
+                    }
+                }
+            } while (parar == true);
+        }
         System.out.println(lista.toString());
     }
-    }
+}
